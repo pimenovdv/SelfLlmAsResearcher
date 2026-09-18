0161591d067c533529387839545f6a2e9b00c3a4
pimenovdv <25670278+pimenovdv@users.noreply.github.com>
2026-09-18 03:28:04 +0000

    Add compute_hedges_g_between_models utility and tests

--- tests/test_experiment_utils.py
+++ tests/test_experiment_utils.py
@@ -4118,6 +4118,23 @@ class TestExperimentUtils(unittest.TestCase):
         self.assertGreaterEqual(compute_friedman_statistic_between_models([m1, m2, m3]), 0.0)
         self.assertEqual(compute_friedman_statistic_between_models([m1]), 0.0)
 
+    def test_compute_hedges_g_between_models(self):
+        from src.experiment_utils import compute_hedges_g_between_models
+        import torch
+        import torch.nn as nn
+        class DummyModel(nn.Module):
+            def __init__(self, w):
+                super().__init__()
+                self.fc = nn.Linear(1, 3, bias=False)
+                self.fc.weight.data = torch.tensor(w, dtype=torch.float32).view(-1, 1)
+            def forward(self, x):
+                return self.fc(x)
+
+        m1 = DummyModel([1.0, 2.0, 3.0])
+        m2 = DummyModel([4.0, 5.0, 6.0])
+        self.assertIsInstance(compute_hedges_g_between_models(m1, m2), float)
+        self.assertEqual(compute_hedges_g_between_models(m1, m1), 0.0)
+
 if __name__ == '__main__':
 
 
